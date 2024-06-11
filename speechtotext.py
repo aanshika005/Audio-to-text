f852c389584733468aab0aa52358d08c29122019
aanshika005 <anshikachoudry005@gmail.com>
2024-06-11 11:36:03 -0500

    speech to text model working

--- speechtotext.py
+++ speechtotext.py
@@ -1,1 +1,12 @@
 #model to convert speech to text
+
+from openai import OpenAI
+client = OpenAI(api_key= "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")
+
+audio_file = open("audio.mp3", "rb")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file, 
+  response_format="text"
+)
+print(transcription)
